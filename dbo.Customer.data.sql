038841c367519dcd9f4a07be138d6a64ea92bef7
Elif Tosun <dptosun1@gmail.com>
2022-02-15 10:36:07 +0300

    ReCapProject

--- dbo.Customer.data.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO [dbo].[Customer] ([Id], [UserId], [CompanyName]) VALUES (1, 1, N'BMW Company                                       ')
-INSERT INTO [dbo].[Customer] ([Id], [UserId], [CompanyName]) VALUES (2, 1, N'Audi Company                                      ')
-INSERT INTO [dbo].[Customer] ([Id], [UserId], [CompanyName]) VALUES (3, 2, N'Ford Company                                      ')
-INSERT INTO [dbo].[Customer] ([Id], [UserId], [CompanyName]) VALUES (4, 2, N'Ford Company                                      ')
-INSERT INTO [dbo].[Customer] ([Id], [UserId], [CompanyName]) VALUES (5, 3, N'BMW Company                                       ')
