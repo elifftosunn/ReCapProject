6fa82e5eb433f97084da0829315373953e5ded60
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-18 20:50:49 +0300

    Update ValidationTool.cs

--- Core/CrossCuttingConcerns/Validation/ValidationTool.cs
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -7,7 +7,7 @@ namespace Core.CrossCuttingConcerns.Validation
 {
     public static class ValidationTool
     {
-        public static void Validate(IValidator validator,object entity) //validator => Car,User object => entity,dto
+        public static void Validate(IValidator validator,object entity) 
         {
             var context = new ValidationContext<object>(entity);
             var result = validator.Validate(context);
