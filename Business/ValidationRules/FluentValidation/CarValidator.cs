35668f6270896d99badff6db37b66828314045c2
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-18 20:44:27 +0300

    Update CarValidator.cs

--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -10,10 +10,10 @@ namespace Business.ValidationRules.FluentValidation
     {
         public CarValidator()
         {
-            RuleFor(c => c.Description).MinimumLength(2).WithMessage("Araba tanımlaması min 2 karakter olmalıdır.");
-            RuleFor(c => c.Description).NotEmpty().WithMessage("Araba tanımı boş bırakılamaz");
-            RuleFor(c => c.DailyPrice).NotEmpty().WithMessage("Araba günlük fiyatı boş bırakılamaz.");
-            RuleFor(c => c.DailyPrice).GreaterThan(100).WithMessage("Araba günlük fiyatı 100'den büyük olmalıdır.");
+            RuleFor(c => c.Description).MinimumLength(2);
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(100);
         }
     }
 }
