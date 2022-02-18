fc07e0dce88ddc9ad51e812520f4c390235a4380
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-18 20:52:59 +0300

    Update MethodInterception.cs

--- Core/Utilities/Interceptors/MethodInterception.cs
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -7,19 +7,18 @@ namespace Core.Utilities.Interceptors
 {
     public abstract class MethodInterception : MethodInterceptionBaseAttribute
     {
-        protected virtual void OnBefore(IInvocation invocation) { } //invocation => BENİM  GÖNDERDİĞİM METHOD
+        protected virtual void OnBefore(IInvocation invocation) { } 
         protected virtual void OnAfter(IInvocation invocation) { }
         protected virtual void OnException(IInvocation invocation, System.Exception e) { }
         protected virtual void OnSuccess(IInvocation invocation) { }
         public override void Intercept(IInvocation invocation)
         {
-            //METHODLARI ÇALIŞTIRMADAN ÖNCE HEPSİ BU KURALLARDAN GEÇİCEK
+            
             var isSuccess = true;
             OnBefore(invocation);
             try
             {
                 invocation.Proceed();
-                //istek yapılan metodu çağırmak istersek IInvocation tipi içerisinde bulunan Proceed
             }
             catch (Exception e)
             {
@@ -27,7 +26,7 @@ namespace Core.Utilities.Interceptors
                 OnException(invocation, e);
                 throw;
             }
-            finally //İSTER ÇALIŞSIN İSTER HATA VERSİN FİNALLY ÇALIŞIR
+            finally
             {
                 if (isSuccess)
                 {
